88c9867c96185e6dc36a47cae734b72aefe37278
Suvashish Shrestha <shresthasuvashish@gmail.com>
2024-08-24 15:05:20 +0545

    initial

---
+++ sand.js
@@ -0,0 +1,12 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", () => {
+  // console.log(request, request.readyState);
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request, request.responseText);
+  } else if (request.readyState === 4) {
+    console.log("COULD NOT FETCH THE DATA");
+  }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
+request.send();
